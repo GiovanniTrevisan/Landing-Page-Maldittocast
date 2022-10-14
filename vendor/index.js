1a33a4b7887b9e1a5f4b6e7d13ce307663429abe
Giovanni B. Trevisan <54962043+GiovanniTrevisan@users.noreply.github.com>
2022-10-14 02:52:53 -0300

    Enhance Website

--- vendor/index.js
+++
@@ -1,7 +0,0 @@
-$(window).scroll(function() {
-  if ($(document).scrollTop() > 100) {
-    $('.navbar').addClass('color-change');
-  } else {
-    $('.navbar').removeClass('color-change');
-  }
-});
